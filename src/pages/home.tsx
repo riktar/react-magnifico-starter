c7621d9bdce1ec00af4168cb0342cf5115fbc96a
Riccardo Tartaglia <tartaglia.riccardo@gmail.com>
2020-09-04 11:31:12 +0200

    initial commit

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import Foo from "../components/Foo/Foo";
 
 const Home: React.FC = () => {
   return (
     <>
       <div className="flex flex-col h-screen items-center justify-center">
         <h1 className="text-5xl text-primary-500 text-center">Magnifico React Starter</h1>
-        <p className="text-gray-700">React + TypeScript + MobX + Tailwind = <strong><Foo bar={'❤️'}/></strong></p>
+        <p className="text-gray-700">React + TypeScript + MobX + Tailwind = ❤</p>
       </div>
     </>
   );
--- src/pages/http404.tsx
+++ src/pages/http404.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Http404: React.FC = () => {
   return (
-    <div style={{
-      display: 'flex',
-      justifyContent: 'center',
-      marginTop: 30
-    }}>
-      <h1>Not found</h1>
+    <div className="flex flex-col h-screen items-center justify-center">
+      <h1 className="text-5xl text-primary-500 text-center">Ops... not Found</h1>
+      <p className="text-gray-700">Are you lost? Go to <Link to={'/'}><span className="text-secondary-500">Home</span></Link></p>
     </div>
   )
 }
